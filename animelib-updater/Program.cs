cbc19d74bc21f6e09b453175fb3ab06d64ad4bf3
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-01-06 19:48:12 +0100

    Add project files.

---
+++ animelib-updater/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animelib_updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var archive = ZipFile.OpenRead("Update.zip");
+            foreach(var v in archive.Entries)
+            {
+                v.ExtractToFile(v.Name, true);
+            }
+        }
+    }
+}
